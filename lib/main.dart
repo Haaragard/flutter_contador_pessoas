55fbab83a180bc4b82073e79e2220382bd9ac967
Haaragard <diogololz159@gmail.com>
2020-09-13 16:25:46 -0300

    Layout terminado

--- lib/main.dart
+++ lib/main.dart
@@ -3,41 +3,50 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "Contador de Pessoas",
-    home: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+    home: Stack(
+      fit: StackFit.expand,
       children: <Widget>[
-        Text("Pessoas: 00",
-            style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold
-            ),
+        Image.asset(
+          "images/restaurant.jpg",
+          fit: BoxFit.cover,
         ),
-        Padding(padding: EdgeInsets.all(10.0),),
-        Row(
+        Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            FlatButton(
-              child: Text("+1",
-                style: TextStyle(fontSize: 40.0, color: Colors.white),
+            Text("Pessoas: 00",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
               ),
-              onPressed: () {},
             ),
-            FlatButton(
-              child: Text("-1",
-                style: TextStyle(fontSize: 40.0, color: Colors.white),
+            Padding(padding: EdgeInsets.all(10.0),),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FlatButton(
+                  child: Text("+1",
+                    style: TextStyle(fontSize: 40.0, color: Colors.white),
+                  ),
+                  onPressed: () {},
+                ),
+                FlatButton(
+                  child: Text("-1",
+                    style: TextStyle(fontSize: 40.0, color: Colors.white),
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            Padding(padding: EdgeInsets.all(10.0),),
+            Text("Pode entrar!",
+              style: TextStyle(
+                color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontSize: 30.0,
               ),
-              onPressed: () {},
+
             ),
           ],
-        ),
-        Padding(padding: EdgeInsets.all(10.0),),
-        Text("Pode entrar!",
-          style: TextStyle(
-              color: Colors.white,
-              fontStyle: FontStyle.italic,
-              fontSize: 30.0,
-          ),
-
         ),
       ],
     ),
